36fc75b4edcdb351de8385269d048c341ed1d2ad
Joshua Benavides <joshua.benavides@verizon.com>
2025-04-30 16:15:39 -0500

    first stretch done

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 // Write your code here
-const lightbulb1 = document.querySelector(".item.lightbulb.active#lightbulb1")
-const  lightbulb2 = document.querySelector(".item.lightbulb#lightbulb2")
-const lightbulb3 = document.querySelector(".item.lightbulb#lightbulb3")
+const lightbulb1 = document.querySelector("#lightbulb1")
+const  lightbulb2 = document.querySelector("#lightbulb2")
+const lightbulb3 = document.querySelector("#lightbulb3")
 
 const output = document.querySelector(".subtitle")
 let count =0;
 lightbulb1.addEventListener("click",function(){
     count++
+    if(count === 1){
+    output.innerHTML = "You've clicked the light " + count + " time"
+    lightbulb1.classList.toggle("active");
+    }
+    else {
     output.innerHTML = "You've clicked the lights " + count + " times"
-    this.classList.toggle("active");
+    lightbulb1.classList.toggle("active");
+    }
    })
 lightbulb2.addEventListener("click",function(){
     count++
-    output.innerHTML = "You've clicked the lights " + count + " times"
-    this.classList.toggle("active");
+    if(count === 1){
+        output.innerHTML = "You've clicked the light " + count + " time"
+        lightbulb2.classList.toggle("active");
+        }
+        else {
+        output.innerHTML = "You've clicked the lights " + count + " times"
+        lightbulb2.classList.toggle("active");
+        }
    })
 lightbulb3.addEventListener("click",function(){
     count++
-    output.innerHTML = "You've clicked the lights " + count + " times"
-    this.classList.toggle("active");
+    if(count === 1){
+        output.innerHTML = "You've clicked the light " + count + " time"
+        lightbulb3.classList.toggle("active");
+        }
+        else {
+        output.innerHTML = "You've clicked the lights " + count + " times"
+        lightbulb3.classList.toggle("active");
+        }
    })
    
